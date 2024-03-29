43907af0f8985686e1f79b5cf2a795f7c28264fc
larryp89 <42215520+larryp89@users.noreply.github.com>
2024-03-29 11:19:11 +0000

    Final version. Updated global variable.

--- main.js
+++ main.js
@@ -1,6 +1,45 @@
+player_score = 0;
+computer_score = 0;
 
-function getComputerChoice{
-    // Generate a random computer move 1-3
-    let random_choice = Math.random(0,2)
-    return random_choice
+function getComputerChoice() {
+  // Generate a random computer move
+  let random_choice = Math.floor(Math.random() * 3) + 1;
+
+  if (random_choice == 1) {
+    return "rock";
+  } else if (random_choice == 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
+
+function compareChoice(playerSelection, computerSelection) {
+  // compare player choice and computer choice
+  if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
+  ) {
+    player_score += 1;
+    return `You win - ${playerSelection} beats ${computerSelection}.`;
+  } else if (playerSelection == computerSelection) {
+    return `You both chose ${playerSelection} - it' a tie.`;
+  } else {
+    computer_score += 1;
+    return `You lost - computer chose ${computerSelection} and you played ${playerSelection}.`;
+  }
+}
+
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt(
+      "Choose 'Rock', 'Paper', or 'Scissors'"
+    ).toLowerCase();
+    let computerSelection = getComputerChoice();
+    console.log(compareChoice(playerSelection, computerSelection));
+  }
+  console.log(`Final score: ${player_score}: ${computer_score}`);
+}
+
+playGame();
